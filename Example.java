4157dafaee6a4bb5a182bdd0060ba7f72be1109e
Kate Holdener <kate.holdener@slu.edu>
2024-02-02 10:45:01 -0600

    added example of array and ArrayList

---
+++ Example.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Example {
+
+    public static void main(String []args) {
+        int []values = new int[10];
+        System.out.println("Length of values array is: " + values.length);
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
+        for (int i = 0; i < values.length; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        System.out.println("Length of values array is: " + values.length);
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        System.out.println("Length of numbers array list is: " + numbers.size());
+        for (int i = 0; i < 10; i++) {
+            numbers.add(i);
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + " ");
+        }
+        System.out.println("");
+        System.out.println("Length of numbers array list is: " + numbers.size());
+
+    }
+}
